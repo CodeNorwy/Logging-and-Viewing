c48b883e2337a8f5e467e1ebe310b26d06080d20
Eirik Illing <213537@usn.no>
2021-05-06 11:22:45 +0200

    Minor changes to UI

--- OPC_Client_DB_Logging_0/UiForm.Designer.cs
+++ OPC_Client_DB_Logging_0/UiForm.Designer.cs
@@ -30,42 +30,42 @@ namespace OPC_Client_DB_Logging_0
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cmbTagId = new System.Windows.Forms.ComboBox();
             this.dgvSensorLog = new System.Windows.Forms.DataGridView();
             this.chrtSensorData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.waveformGraph1 = new NationalInstruments.UI.WindowsForms.WaveformGraph();
+            this.waveformPlot1 = new NationalInstruments.UI.WaveformPlot();
+            this.xAxis1 = new NationalInstruments.UI.XAxis();
+            this.yAxis1 = new NationalInstruments.UI.YAxis();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReadType = new System.Windows.Forms.TextBox();
+            this.txtReadQuality = new System.Windows.Forms.TextBox();
+            this.txtReadValue = new System.Windows.Forms.TextBox();
             this.lblStatusOPC = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtConStrOPC = new System.Windows.Forms.TextBox();
             this.btnLog = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.txtReadValue = new System.Windows.Forms.TextBox();
-            this.txtReadQuality = new System.Windows.Forms.TextBox();
-            this.txtReadType = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbLogInterval = new System.Windows.Forms.ComboBox();
+            this.lblStatusDB = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txtConStringDB = new System.Windows.Forms.TextBox();
-            this.lblStatusDB = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.waveformGraph1 = new NationalInstruments.UI.WindowsForms.WaveformGraph();
-            this.xAxis1 = new NationalInstruments.UI.XAxis();
-            this.yAxis1 = new NationalInstruments.UI.YAxis();
-            this.waveformPlot1 = new NationalInstruments.UI.WaveformPlot();
-            this.cmbLogInterval = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSensorLog)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chrtSensorData)).BeginInit();
             this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).BeginInit();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -73,7 +73,7 @@ namespace OPC_Client_DB_Logging_0
             this.groupBox1.Controls.Add(this.cmbTagId);
             this.groupBox1.Controls.Add(this.dgvSensorLog);
             this.groupBox1.Controls.Add(this.chrtSensorData);
-            this.groupBox1.Location = new System.Drawing.Point(835, 12);
+            this.groupBox1.Location = new System.Drawing.Point(705, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(678, 537);
             this.groupBox1.TabIndex = 0;
@@ -102,16 +102,16 @@ namespace OPC_Client_DB_Logging_0
             // 
             // chrtSensorData
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chrtSensorData.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chrtSensorData.Legends.Add(legend4);
+            chartArea1.Name = "ChartArea1";
+            this.chrtSensorData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chrtSensorData.Legends.Add(legend1);
             this.chrtSensorData.Location = new System.Drawing.Point(6, 48);
             this.chrtSensorData.Name = "chrtSensorData";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chrtSensorData.Series.Add(series4);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chrtSensorData.Series.Add(series1);
             this.chrtSensorData.Size = new System.Drawing.Size(666, 279);
             this.chrtSensorData.TabIndex = 1;
             this.chrtSensorData.Text = "chart2";
@@ -134,6 +134,77 @@ namespace OPC_Client_DB_Logging_0
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "OPC Connection";
+            // 
+            // waveformGraph1
+            // 
+            this.waveformGraph1.Location = new System.Drawing.Point(240, 99);
+            this.waveformGraph1.Name = "waveformGraph1";
+            this.waveformGraph1.Plots.AddRange(new NationalInstruments.UI.WaveformPlot[] {
+            this.waveformPlot1});
+            this.waveformGraph1.Size = new System.Drawing.Size(432, 168);
+            this.waveformGraph1.TabIndex = 9;
+            this.waveformGraph1.UseColorGenerator = true;
+            this.waveformGraph1.XAxes.AddRange(new NationalInstruments.UI.XAxis[] {
+            this.xAxis1});
+            this.waveformGraph1.YAxes.AddRange(new NationalInstruments.UI.YAxis[] {
+            this.yAxis1});
+            // 
+            // waveformPlot1
+            // 
+            this.waveformPlot1.XAxis = this.xAxis1;
+            this.waveformPlot1.YAxis = this.yAxis1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TAG TYPE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "TAG QUALITY:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "TAG VALUE:";
+            // 
+            // txtReadType
+            // 
+            this.txtReadType.Location = new System.Drawing.Point(91, 163);
+            this.txtReadType.Multiline = true;
+            this.txtReadType.Name = "txtReadType";
+            this.txtReadType.ReadOnly = true;
+            this.txtReadType.Size = new System.Drawing.Size(133, 53);
+            this.txtReadType.TabIndex = 5;
+            // 
+            // txtReadQuality
+            // 
+            this.txtReadQuality.Location = new System.Drawing.Point(91, 131);
+            this.txtReadQuality.Name = "txtReadQuality";
+            this.txtReadQuality.ReadOnly = true;
+            this.txtReadQuality.Size = new System.Drawing.Size(133, 20);
+            this.txtReadQuality.TabIndex = 4;
+            // 
+            // txtReadValue
+            // 
+            this.txtReadValue.Location = new System.Drawing.Point(91, 99);
+            this.txtReadValue.Name = "txtReadValue";
+            this.txtReadValue.ReadOnly = true;
+            this.txtReadValue.Size = new System.Drawing.Size(133, 20);
+            this.txtReadValue.TabIndex = 3;
             // 
             // lblStatusOPC
             // 
@@ -178,58 +249,6 @@ namespace OPC_Client_DB_Logging_0
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // txtReadValue
-            // 
-            this.txtReadValue.Location = new System.Drawing.Point(91, 99);
-            this.txtReadValue.Name = "txtReadValue";
-            this.txtReadValue.ReadOnly = true;
-            this.txtReadValue.Size = new System.Drawing.Size(133, 20);
-            this.txtReadValue.TabIndex = 3;
-            // 
-            // txtReadQuality
-            // 
-            this.txtReadQuality.Location = new System.Drawing.Point(91, 131);
-            this.txtReadQuality.Name = "txtReadQuality";
-            this.txtReadQuality.ReadOnly = true;
-            this.txtReadQuality.Size = new System.Drawing.Size(133, 20);
-            this.txtReadQuality.TabIndex = 4;
-            // 
-            // txtReadType
-            // 
-            this.txtReadType.Location = new System.Drawing.Point(91, 163);
-            this.txtReadType.Multiline = true;
-            this.txtReadType.Name = "txtReadType";
-            this.txtReadType.ReadOnly = true;
-            this.txtReadType.Size = new System.Drawing.Size(133, 53);
-            this.txtReadType.TabIndex = 5;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 102);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "TAG VALUE:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 133);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "TAG QUALITY:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 164);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "TAG TYPE:";
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.label6);
@@ -244,6 +263,39 @@ namespace OPC_Client_DB_Logging_0
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Logging Settings";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(279, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "LOGGING TIME milliseconds:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // cmbLogInterval
+            // 
+            this.cmbLogInterval.FormattingEnabled = true;
+            this.cmbLogInterval.Items.AddRange(new object[] {
+            "500",
+            "1000",
+            "1500",
+            "2000"});
+            this.cmbLogInterval.Location = new System.Drawing.Point(311, 132);
+            this.cmbLogInterval.Name = "cmbLogInterval";
+            this.cmbLogInterval.Size = new System.Drawing.Size(82, 21);
+            this.cmbLogInterval.TabIndex = 11;
+            this.cmbLogInterval.SelectedIndexChanged += new System.EventHandler(this.cmbLogInterval_SelectedIndexChanged);
+            // 
+            // lblStatusDB
+            // 
+            this.lblStatusDB.AutoSize = true;
+            this.lblStatusDB.Location = new System.Drawing.Point(6, 73);
+            this.lblStatusDB.Name = "lblStatusDB";
+            this.lblStatusDB.Size = new System.Drawing.Size(79, 13);
+            this.lblStatusDB.TabIndex = 4;
+            this.lblStatusDB.Text = "Not Connected";
             // 
             // label5
             // 
@@ -262,68 +314,16 @@ namespace OPC_Client_DB_Logging_0
             this.txtConStringDB.Size = new System.Drawing.Size(666, 20);
             this.txtConStringDB.TabIndex = 9;
             // 
-            // lblStatusDB
-            // 
-            this.lblStatusDB.AutoSize = true;
-            this.lblStatusDB.Location = new System.Drawing.Point(6, 73);
-            this.lblStatusDB.Name = "lblStatusDB";
-            this.lblStatusDB.Size = new System.Drawing.Size(79, 13);
-            this.lblStatusDB.TabIndex = 4;
-            this.lblStatusDB.Text = "Not Connected";
-            // 
             // timer2
             // 
             this.timer2.Interval = 500;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
-            // waveformGraph1
-            // 
-            this.waveformGraph1.Location = new System.Drawing.Point(240, 99);
-            this.waveformGraph1.Name = "waveformGraph1";
-            this.waveformGraph1.Plots.AddRange(new NationalInstruments.UI.WaveformPlot[] {
-            this.waveformPlot1});
-            this.waveformGraph1.Size = new System.Drawing.Size(432, 168);
-            this.waveformGraph1.TabIndex = 9;
-            this.waveformGraph1.UseColorGenerator = true;
-            this.waveformGraph1.XAxes.AddRange(new NationalInstruments.UI.XAxis[] {
-            this.xAxis1});
-            this.waveformGraph1.YAxes.AddRange(new NationalInstruments.UI.YAxis[] {
-            this.yAxis1});
-            // 
-            // waveformPlot1
-            // 
-            this.waveformPlot1.XAxis = this.xAxis1;
-            this.waveformPlot1.YAxis = this.yAxis1;
-            // 
-            // cmbLogInterval
-            // 
-            this.cmbLogInterval.FormattingEnabled = true;
-            this.cmbLogInterval.Items.AddRange(new object[] {
-            "500",
-            "1000",
-            "1500",
-            "2000"});
-            this.cmbLogInterval.Location = new System.Drawing.Point(311, 132);
-            this.cmbLogInterval.Name = "cmbLogInterval";
-            this.cmbLogInterval.Size = new System.Drawing.Size(82, 21);
-            this.cmbLogInterval.TabIndex = 11;
-            this.cmbLogInterval.SelectedIndexChanged += new System.EventHandler(this.cmbLogInterval_SelectedIndexChanged);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(279, 110);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(147, 13);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "LOGGING TIME milliseconds:";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
             // UiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1525, 559);
+            this.ClientSize = new System.Drawing.Size(1397, 559);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -334,9 +334,9 @@ namespace OPC_Client_DB_Logging_0
             ((System.ComponentModel.ISupportInitialize)(this.chrtSensorData)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).EndInit();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).EndInit();
             this.ResumeLayout(false);
 
         }
